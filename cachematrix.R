39664f7a43220fb93649e11d57309a141e71afac
awilliea <awillieloua@gmail.com>
2017-01-18 20:44:57 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+##The two functions below can cache a inverse matrix of 'x',and next time if we 
+##need the inverse matrix of 'x' we can call it instead of computing it again.
 ## Write a short comment describing this function
-
+##The function makeCacheMatrix can cache a matrix 'x'.
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -20,7 +21,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##The function cacheSolve can calculate the inverse matrix of 'x',and it can tell
+##us whether we have done it before.
+##If we have done it ,we can get the value of the inverse matrix of 'x' instead of
+##computing it again.
 cacheSolve <- function(x, ...) { 
         m <- x$getinverse()
         if(!is.null(m)) {
